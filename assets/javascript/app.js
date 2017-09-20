2190a7146c84f92a92cfb995e64d7fe8c0dc0943
HelloBetafish <bethany.pfeister@gmail.com>
2017-09-20 13:31:17 -0700

    basic display page, working with Destiny 2 api to display info on site

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$(document.body).on("click","#info-btn", function() {
+  var apikey = "6b0538962054452e841ece2b6229c1d1";
+  var queryURL = "http://www.bungie.net/Platform/Destiny2/Manifest/";
+
+  // Performing our AJAX GET request
+  $.ajax({
+  	url: queryURL,
+  	method: "GET",
+  	Host: "www.bungie.net",
+  	Connection: "keep-alive",
+  	X-API-Key: apikey,
+  	Cookie: "bungledid=B6BGVMQFOKdJsTAWEnsW/ko5xn4glmfRCAAA; bungled=2796665744958383183" 
+
+  })
+  // After the data comes back from the API
+  .done(function(response){
+  	console.log(results);
+  });
+});
